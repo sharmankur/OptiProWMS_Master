db2a053ca38ab5af59a2bb3d7cfadd3df182b3ff
rishabh05 <rishabhjainwit5>
2020-01-24 15:23:56 +0530

    commiting files

--- src/app/account/signin/signin.component.ts
+++ src/app/account/signin/signin.component.ts
@@ -156,6 +156,7 @@ export class SigninComponent implements OnInit {
             // localStorage.setItem("GUID", this.licenseData[1].GUID);
             localStorage.setItem("CompID", "DEVSCHEDULING");
             localStorage.setItem("whseId", "01");
+            localStorage.setItem("UserId", this.userName);
             localStorage.setItem("Token", "2bf91be7-819c-4443-a1bc-82dc150da05d");
 
             localStorage.setItem("DecimalPrecision", "2");
